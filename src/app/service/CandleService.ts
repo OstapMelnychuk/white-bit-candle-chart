51f033cef77a098bf0eb0cb97b88faeb9432e15c
OstapMelnychuk <ostapss222@gmail.com>
2025-10-28 18:30:06 +0200

    Changed urls to BE server due to computer restart seconf time

--- src/app/service/CandleService.ts
+++ src/app/service/CandleService.ts
@@ -7,7 +7,7 @@ import {Candle} from '../models/Candle';
   providedIn: 'root'
 })
 export class CandleService {
-  private baseUrl = 'https://600195826587.ngrok-free.app/api/trade/candle';
+  private baseUrl = 'https://b68ce9830406.ngrok-free.app/api/trade/candle';
 
   constructor(private http: HttpClient) {}
 
--- src/app/service/CandleWsService.ts
+++ src/app/service/CandleWsService.ts
@@ -9,7 +9,7 @@ import { Candle } from '../models/Candle';
 
 @Injectable({ providedIn: 'root' })
 export class CandleWsService {
-  private socketUrl = 'https://600195826587.ngrok-free.app/ws/candles'; // your STOMP endpoint
+  private socketUrl = 'https://b68ce9830406.ngrok-free.app/ws/candles'; // your STOMP endpoint
   private client!: Client;
   private _candleUpdates = new BehaviorSubject<Candle | null>(null);
 
